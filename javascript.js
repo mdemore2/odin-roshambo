d89a8acf45e23c2042d206fbca35ee062642133a
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-01 14:05:35 -0400

    Completed playRound function in javascript.js

--- javascript.js
+++ javascript.js
@@ -4,4 +4,43 @@ function getComputerChoice() {
     return choices[(Math.floor(Math.random() * choices.length))];
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    if (playerSelection === computerSelection) {
+        return 'Draw!';
+    }
+    else if (playerSelection === 'ROCK') {
+        if (computerSelection === 'SCISSORS')
+        {
+            return 'You win! Rock beats scissors!';
+        }
+        else if (computerSelection === 'PAPER')
+        {
+            return 'You lose! Paper covers rock!';
+        }
+    }
+    else if (playerSelection === 'PAPER') {
+        if (computerSelection === 'ROCK'){
+            return 'You win! Paper covers rock!';
+        }
+        else if (computerSelection === 'SCISSORS'){
+            return 'You lose! Scissors cut paper!';
+        }
+    }
+    else if (playerSelection === 'SCISSORS'){
+        if (computerSelection === 'ROCK'){
+            return 'You lose! Rock beats scissors!';
+        }
+        else if (computerSelection === 'PAPER'){
+            return 'You win! Scissors cut paper!';
+        }
+    }
+}
+
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
